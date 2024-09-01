880830dadd8710ee486d6b3c686c9960f02b11b3
zylarlab <maruf.hasan.taposh@g.bracu.ac.bd>
2024-09-01 21:16:22 +0600

    wrapper

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,15 +2,17 @@
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 import Home from '../app/home/page.js';
-
+import { QueryClientProviderWrapper } from '../lib/QueryClientProvider';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         
-        
+        <QueryClientProviderWrapper>
         <main>{children}</main>
+        </QueryClientProviderWrapper>
+        
         
       </body>
     </html>
